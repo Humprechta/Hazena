b7082d753692d792cc67e178dcf6d72bfa399ade
Humprechta <91117513+Humprechta@users.noreply.github.com>
2022-01-06 11:35:38 +0100

    Add files via upload

--- HZv2.3/index_js.js
+++ HZv2.3/index_js.js
@@ -1,8 +1,10 @@
 
+
+
 /*navigation bar*/
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className == "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
@@ -42,34 +44,22 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2022 00:00:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+//scroll on top
+var mybutton = document.getElementById("nahoruu");
 
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-   
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "Zápas nyní probíhá";
-  }if (distance < -3600) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "Zápas již proběhl...";
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-}, 1000);
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
