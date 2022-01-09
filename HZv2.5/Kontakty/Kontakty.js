2c7495036bf7821f39f3208b542e5f794ff7ab67
Humprechta <91117513+Humprechta@users.noreply.github.com>
2022-01-09 17:29:46 +0100

    Add files via upload

---
+++ HZv2.5/Kontakty/Kontakty.js
@@ -0,0 +1,63 @@
+
+/*navigation bar*/
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className == "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/*GALLERY*/
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("imgss");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+  for (var i = 0; i < img.length; i++) {
+    img[i].onclick = function(){
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
+	}
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+document.getElementsByClassName("close")[0].style.zIndex = "13";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+//side open
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.zIndex = "99";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+//scroll on top
+var mybutton = document.getElementById("nahoruu");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
